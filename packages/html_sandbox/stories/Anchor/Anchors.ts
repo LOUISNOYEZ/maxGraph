055048fb1a11044725a3a5269c31b2474a2f6cee
lnoyez <louis.noyez@gmail.com>
2025-12-03 05:51:05 +0100

    feat: Migrate Anchors story to CSF3 Creates separate Anchors file to isolate maxGraph specifics and to declare an AnchorsDemo component (function that returns a container HTMLElement linked to a Graph).

--- packages/html_sandbox/stories/Anchor/Anchors.ts
+++ packages/html_sandbox/stories/Anchor/Anchors.ts
@@ -13,4 +13,96 @@ import {
   SelectionHandler,
 } from '@maxgraph/core';
 
+import { configureImagesBasePath, createGraphContainer } from '../shared/configure.js';
 
+// style required by RubberBand
+import '@maxgraph/core/css/common.css';
+
+const anchorsPluginsDefault: GraphPluginConstructor[] = [
+  CellEditorHandler,
+  SelectionCellsHandler,
+  ConnectionHandler,
+  SelectionHandler,
+];
+
+export class AnchorsGraph extends Graph {
+  constructor(container: HTMLElement, rubberBand: boolean = true) {
+    const plugins = rubberBand
+      ? anchorsPluginsDefault.concat(RubberBandHandler)
+      : anchorsPluginsDefault;
+    super(container, undefined, plugins);
+
+    this.getPlugin<ConnectionHandler>('ConnectionHandler')!.createEdgeState = function ( _me: InternalMouseEvent ) {
+      const edge = this.graph.createEdge(null, null!, null, null, null);
+      return new CellState(this.graph.view, edge, this.graph.getCellStyle(edge));
+    }
+
+    this.setConnectable(true);
+
+    // Specifies the default edge style
+    this.getStylesheet().getDefaultEdgeStyle().edgeStyle = 'orthogonalEdgeStyle';
+  }
+}
+
+export const AnchorsDemo = (args: Record<string, string>) => {
+  configureImagesBasePath();
+  const container = createGraphContainer(args);
+
+  class AnchorsGeometryClass extends Geometry {
+    // Defines the default constraints for the vertices
+    constraints = [
+      new ConnectionConstraint(new Point(0.25, 0), true),
+      new ConnectionConstraint(new Point(0.5, 0), true),
+      new ConnectionConstraint(new Point(0.75, 0), true),
+      new ConnectionConstraint(new Point(0, 0.25), true),
+      new ConnectionConstraint(new Point(0, 0.5), true),
+      new ConnectionConstraint(new Point(0, 0.75), true),
+      new ConnectionConstraint(new Point(1, 0.25), true),
+      new ConnectionConstraint(new Point(1, 0.5), true),
+      new ConnectionConstraint(new Point(1, 0.75), true),
+      new ConnectionConstraint(new Point(0.25, 1), true),
+      new ConnectionConstraint(new Point(0.5, 1), true),
+      new ConnectionConstraint(new Point(0.75, 1), true),
+    ];
+  }
+
+  // Creates the graph inside the given container
+  const graph: AnchorsGraph = new AnchorsGraph(container, Boolean(args.rubberBand));
+
+  graph.getAllConnectionConstraints = (terminal: CellState | null, _source: boolean) => {
+    // Overridden to define per-geometry connection points
+    return (terminal?.cell?.geometry as AnchorsGeometryClass)?.constraints ?? null;
+  };
+
+  // Gets the default parent for inserting new cells. This
+  // is normally the first child of the root (ie. layer 0).
+  const parent = graph.getDefaultParent();
+
+  // Adds cells to the model in a single step
+  graph.batchUpdate(() => {
+    const v1 = graph.insertVertex({
+      parent,
+      value: 'Hello,',
+      position: [20, 20],
+      size: [80, 30],
+      geometryClass: AnchorsGeometryClass,
+    });
+    const v2 = graph.insertVertex({
+      parent,
+      value: 'World!',
+      position: [200, 150],
+      size: [80, 30],
+      geometryClass: AnchorsGeometryClass,
+    });
+    graph.insertEdge({
+      parent,
+      value: '',
+      source: v1,
+      target: v2,
+    });
+  });
+
+  return container;
+};
+
+export default AnchorsDemo;
